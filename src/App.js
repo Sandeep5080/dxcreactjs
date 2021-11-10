7157e80eb4537a386b81b4d1101de491b917a8c6
Your Sandeep5080 <sandeepreddy4366@gmail.com>
2021-11-10 08:48:56 +0530

    pass a ref of a method as props Child

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 
 
@@ -14,6 +15,9 @@ import EventBind from './components/EventBind';
 function App() {
   return (
     <div className="App">
+
+
+      <ParentComponent/>
       <EventBind/>
       <ClassClick/>
       <FunctionClick/>
---
+++ src/components/ParentComponent.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import ChildComponent from './ChildComponent'
+
+export class ParentComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            parentName: 'rajesh'
+        }
+        this.greetParent = this.greetParent.bind(this)
+    }
+    greetParent(){
+        
+        alert(`hello ${this.state.parentName}`) 
+    }
+    render() {
+        return (
+            <div>
+                <ChildComponent rameshHandler = {this.greetParent}/>  
+            </div>
+        )
+    }
+}
+
+export default ParentComponent
