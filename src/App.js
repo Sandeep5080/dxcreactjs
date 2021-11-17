c4db3ecfb29e5301b8eb63fd9537f387c7bc9830
Your Sandeep5080 <sandeepreddy4366@gmail.com>
2021-11-17 16:43:10 +0530

    run effects only once

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ import HookCounter from './components/hooks/HookCounter';
 import HookCounter2 from './components/hooks/HookCounter2';
 import HookCounter3 from './components/hooks/HookCounter3';
 import CounterHook1 from './components/hooks/useeffect/CounterHook1';
+import HookMouse from './components/hooks/useeffect/HookMouse';
  
 
 
@@ -38,7 +39,7 @@ import CounterHook1 from './components/hooks/useeffect/CounterHook1';
 function App() {
   return (
     <div className="App">
-      <CounterHook1/>
+      <HookMouse/>
       
 
     </div>
