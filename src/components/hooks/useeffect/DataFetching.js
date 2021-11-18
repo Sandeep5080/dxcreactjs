83ce344a572fcdd497c2c3ca431245abeac48649
Your Sandeep5080 <sandeepreddy4366@gmail.com>
2021-11-18 11:45:13 +0530

    getting only one post

--- src/components/hooks/useeffect/DataFetching.js
+++ src/components/hooks/useeffect/DataFetching.js
@@ -2,31 +2,37 @@ import React, {useState,useEffect} from 'react'
 import axios from 'axios'
 
 function DataFetching() {
+    const [post,setPost] = useState({})
+    const [id,setId] = useState(1)
 
-    const [posts,setPosts] = useState([])
-    //Promise -- Deferred obj in kotlin--
+    
     useEffect(
         ()=> {
             axios.get('https://jsonplaceholder.typicode.com/posts')
                 .then(res => {
                     console.log(res)
-                    setPosts(res.data)
+                    setPost(res.data)
                 })
                 .catch(err =>{
                     console.log(err)
                 })
-        }
+        },
+        [id]
     )
     
         return (
             <div>
-                <ul>
-                {
-                    posts.map(
-                        post => <li key={post.id}> {post.title}</li>
-                    )
-                }
-                </ul>
+                <input 
+            type = "text"
+            value = {id}
+            onChange = { e => setId(e.target.value)}
+            />
+            <div>{post.title}</div>
+           {/* <ul>{
+                posts.map(
+                    post => <li key = {post.id}>{post.title}</li>
+                )
+            }</ul>*/}
             </div>
         )
     }
